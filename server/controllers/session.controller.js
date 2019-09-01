70e85fffd8562d3a1ef5c52fa4668c4fec489c5d
irakoze yves <irakozeyves1@gmail.com>
2019-09-01 19:33:57 +0200

    ft(session controller)  create sessin controller [#168237286]

---
+++ server/controllers/session.controller.js
@@ -0,0 +1,105 @@
+import { Session, sessions } from '../models/session.model';
+import { reviews } from '../models/review.model';
+//Create a mentorship session request.
+
+export const session = (req, res) => {
+	const session = new Session(sessions.length + 1, req.body.mentorId, req.user.id, req.body.questions, req.user.email);
+	sessions.push(session);
+
+	return res.status(200).send({
+		'status': 200,
+		'data': session
+	});
+};
+
+//A mentor can accept a mentorship session request
+export const accept = (req, res) => {
+	if (req.user.role == 'Mentor') {
+		const accep = sessions.findIndex(a => a.sessionId == req.params.sessionId);
+
+		if (accep > - 1) {
+			sessions[accep].status = 'Accept';
+
+			return res.status(202).send({
+				'status': 202,
+				'data': sessions[accep]
+			});
+		}
+
+		return res.status(404).send({
+			'status': 404,
+			'message': 'No session of this specific id'
+		});
+
+	}
+};
+//A mentor can reject a mentorship session request.
+
+export const reject = (req, res) => {
+	if (req.user.role == 'Mentor') {
+		const rejec = sessions.findIndex(a => a.sessionId == req.params.sessionId);
+
+		if (rejec > -1) {
+			sessions[rejec].status = 'Reject';
+
+			return res.status(200).send({
+				'status': 200,
+				'data': sessions[rejec]
+			});
+		}
+		return res.status(501).send({
+			'status': 501,//Not implemented 501 status
+			'message': 'No session of this specific id'
+		});
+	}
+};
+
+//Get all mentorship session requests
+export const getSessionById = (req, res) => {
+	
+	return res.status(200).send({
+		status: 200,
+		data :{
+			session
+		}
+	});
+	
+};
+
+
+// Admin can delete mentorship session review deemed inappropriate.
+export const remove = (req, res) => {
+	const removes = reviews.findIndex(r => r.sessionId == req.params.sessionId);
+	if(removes > -1){
+		reviews.splice(removes, 1);
+		return res.status(200).send({
+			status: 200,
+			data : {
+				message: 'Review successfully deleted'
+			}
+		});
+	}
+
+
+};
+
+//Review a mentor after a mentorship session.
+export const review = (req, res) => {
+	const s = sessions.find(s => s.sessionId == req.params.sessionId);
+	if (s && s.status != 'Rejected') {
+		const review = new reviews(s.sessionId, s.mentorId, req.user.userId, req.body.score, req.user.menteeFullName, req.body.remark);
+		reviews.push(review);
+		return res.status(201).send({
+			'status': 201,
+			'message': 'Session created successfully',
+			'data': review
+		});
+	} else {
+		return res.status(400).send({
+			'status': 400,
+			'message': 'Bad request'
+		});
+	}
+
+
+};
