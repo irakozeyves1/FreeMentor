b7a7b97ed2880d3da9f0c22ee12f8eb2b9adba58
irakozeyves1 <52418549+irakozeyves1@users.noreply.github.com>
2019-09-01 17:25:08 +0200

    update server
    editi

--- server/server.js
+++ server/server.js
@@ -1,9 +1,6 @@
 import app from './app';
 
-const port = process.env.PORT || 3000;
-
-const server = app.listen(port, () => {
-  console.log(`App running on port ${port}`);
-});
-
+// eslint-disable-next-line no-undef
+const port = process.env.PORT || 5000;
+const server = app.listen(port, () => console.log(`Server is running on port: ${port}`));
 export default server;
