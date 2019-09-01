9db95898dae3755ef95e60f18a713b82f8bf0b54
irakozeyves1 <52418549+irakozeyves1@users.noreply.github.com>
2019-09-01 17:22:35 +0200

    update
    editing element

--- server/app.js
+++ server/app.js
@@ -1,38 +1,13 @@
 import express from 'express';
-// import body-parser for extracting body portion
 import bodyParser from 'body-parser';
-// import the index routes
-import routes from './routes/index';
-// import swagger
-import swaggerUi from 'swagger-ui-express';
-import swaggerDoc from '../swagger.json';
-
+import dotenv from 'dotenv';
+import userRoutes from './routes/user.route';
+dotenv.config();
 
 const app = express();
 
-app.use(bodyParser.urlencoded({ extended: false })); // for only passing strings and arrays
-app.use(bodyParser.json());// accept json data
+app.use(bodyParser.json());
 
-// redirect to the routes
-app.use('/api/v1/', routes);
-
-// redirect to swagger json
-app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDoc));
-
-// Index of autmart
-app.get('/', (req, res) => res.status(200).json({
-	status: 200,
-   	message: 'Welcome to AutoMart, Online shop for cars.'
-}));
-
-// accept static files
-app.use('/ui', express.static('UI'));
-
-app.use((req, res) => {
-  return res.status(404).json({
-    status: 404,
-    message: 'URL not found',
-  });
-});
+app.use('/api/v1', userRoutes);
 
 export default app;
