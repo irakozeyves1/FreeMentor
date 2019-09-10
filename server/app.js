05f7ffd839e5d927bb984135d92424f431ccdd53
irakoze yves <irakozeyves1@gmail.com>
2019-09-10 15:58:08 +0200

    ft(updating app) updating app js[#168404792]

--- server/app.js
+++ server/app.js
@@ -6,6 +6,10 @@ import userRoutes from './routes/user.route';
 import swaggerUi from 'swagger-ui-express';
 import swaggerDoc from './swagger.json';
 
+import Database from './db/db';
+
+Database.createScripts();
+ 
 dotenv.config();
 
 const app = express();
@@ -15,6 +19,15 @@ app.use(bodyParser.json());
 app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDoc));
 
 app.use('/api/v1', userRoutes);
+
+//Index Of FreeMentor 
+
+app.get('/', (req, res) => res.status(200).json({
+	status: 200,
+	message: 'Welcome to FreeMentor, Online FreeMentorship Program.'
+}));
+
+app.use((req, res) => (res.status(404).json({ status: 404, message: 'Route not found' })));
 
 
 export default app;
